3702fe8d3978eaf517c3224237c0fd517e66657f
OIITuMuCT <vataman@live.ru>
2025-08-07 19:46:52 +0300

    Creating a Todos model

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -9,7 +9,7 @@ class Todos(Base):
 
     __tablename__ = "todos"
 
-    id = (Column(Integer, primary_key=True, index=True),)
+    id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     description = Column(String)
     priority = Column(Integer)
