452381f324b8f7e7b6990e59dee71ebe8500ceac
OIITuMuCT <vataman@live.ru>
2025-08-20 13:12:43 +0300

    Adding a test_get_current_user_valid_token, test_current_user_missing_payload

--- TodoApp/tests/test_auth.py
+++ TodoApp/tests/test_auth.py
@@ -1,7 +1,9 @@
 from .utils import *
-from ..routers.auth import get_db, authenticate_user, create_access_token, SECRET_KEY, ALGORITHM
+from ..routers.auth import get_db, authenticate_user, create_access_token, get_current_user, SECRET_KEY, ALGORITHM
 from jose import jwt
 from datetime import timedelta
+import pytest
+from fastapi import HTTPException
 
 app.dependency_overrides[get_db] = override_get_db
 
@@ -31,3 +33,22 @@ def test_create_access_token():
     assert decoded_token['sub'] == username
     assert decoded_token['id'] == user_id
     assert decoded_token['role'] == role
+
+@pytest.mark.asyncio
+async def test_get_current_user_valid_token():
+    encode = {'sub': 'testuser', 'id': 1, 'role': 'admin'}
+    token = jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+    
+    user = await get_current_user(token=token)
+    assert user == {'username': 'testuser', 'id':1, 'user_role': 'admin'}
+    
+@pytest.mark.asyncio
+async def test_get_current_user_missing_payload():
+    encode = {'role': 'user'}
+    token = jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+    
+    with pytest.raises(HTTPException) as excinfo:
+        await get_current_user(token=token)
+    
+    assert excinfo.value.status_code == 401
+    assert excinfo.value.detail == 'Could not validate user.'
