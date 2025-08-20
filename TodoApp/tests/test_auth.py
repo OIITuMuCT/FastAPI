ac5da169bb38137e16a58a083ab2092790e48347
OIITuMuCT <vataman@live.ru>
2025-08-20 12:42:29 +0300

    Adding a test_create_access_token

--- TodoApp/tests/test_auth.py
+++ TodoApp/tests/test_auth.py
@@ -1,5 +1,7 @@
 from .utils import *
-from ..routers.auth import get_db, authenticate_user
+from ..routers.auth import get_db, authenticate_user, create_access_token, SECRET_KEY, ALGORITHM
+from jose import jwt
+from datetime import timedelta
 
 app.dependency_overrides[get_db] = override_get_db
 
@@ -8,10 +10,24 @@ def test_authenticate_user(test_user):
 
     authenticated_user = authenticate_user(test_user.username, 'testpassword', db)
     assert authenticated_user is not None
-    assert authenticated_user.username == test_user.username 
+    assert authenticated_user.username == test_user.username
 
     non_existent_user = authenticate_user('WrongUserName', 'testpassword', db)
     assert non_existent_user is False
-    
+
     wrong_password_user = authenticate_user(test_user.username, 'wrongpassword', db)
     assert wrong_password_user is False
+
+
+def test_create_access_token():
+    username = 'testuser'
+    user_id = 1
+    role = 'user'
+    expires_delta = timedelta(days=1)
+
+    token = create_access_token(username, user_id, role, expires_delta)
+    
+    decoded_token = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM, options={'verify_signature': False})
+    assert decoded_token['sub'] == username
+    assert decoded_token['id'] == user_id
+    assert decoded_token['role'] == role
